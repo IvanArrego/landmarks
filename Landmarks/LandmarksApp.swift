a5d53ee7da3e57a97a64555f8161bed265969ca5
Ivan Arregoitia <ivanarrego@gmail.com>
2021-05-23 20:50:28 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ivan Arregoitia on 5/23/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
